8abdd6aabbdd96f25c420b4c60933092d1db12c1
listingslab <listingslab@gmail.com>
2016-12-16 14:57:50 +1030

    Add initial redux app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import App from './App';
 
+let store = createStore(todoApp);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
