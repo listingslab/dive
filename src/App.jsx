dd093d356b462f7f0decf3668b7b1dc194e2183a
listingslab <listingslab@gmail.com>
2016-12-17 00:48:27 +1030

    Verge viewport dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import verge from 'verge';
 import Example from './components/example';
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
 
-const App = () => (
-  <Example />
-);
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions() {
+    // https://www.npmjs.com/package/verge
+    console.log(verge.viewport());
+    // console.log('Height: ' + verge.viewportH());
+  }
+
+  render() {
+    return (
+      <Example />
+    );
+  }
+}
 
 export default App;
